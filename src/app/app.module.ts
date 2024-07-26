91b07f99041d85b49f8fc63f77447ce04490679a
Rupesh Pandey <rupeshpandey432@gmail.com>
2024-07-26 13:02:22 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { VendorDashboardComponent } from './components/vendor-dashboard/vendor-dashboard.component';
+import { VendorRegistrationComponent } from './components/vendor-registration/vendor-registration.component';
+import { VendorListComponent } from './components/vendor-list/vendor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VendorDashboardComponent,
+    VendorRegistrationComponent,
+    VendorListComponent
   ],
   imports: [
     BrowserModule,
